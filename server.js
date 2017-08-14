cd1abeff6f0e0fbdd4c8d76d18feeccc9f0ae6f9
Admin <admin@milads-MacBook-Pro.local>
2017-08-13 22:50:02 -0400

    server auth

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -7,24 +10,45 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.User.findOne({where:{user_id:username}}).then(function(user) {
+      if (!user) { return cb(null, false); }
+      if (user.user_password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, cb) {
+    db.User.findOne({where:{id:id}}).then(function ( user) {
+      cb(null, user);
+    });
+  });
+
+
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
 
 app.use(express.static("./public"));
 
 //PASSPORT
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-//PASSPORT
-require("./routes/passport.js")(app);
-require("./routes/html-routes.js")(app);
 
-db.sequelize.sync().then(function() {
+require("./routes/html-routes.js")(app,passport);
+
+db.sequelize.sync({force:true}).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
