220f1c119ea3fdc4f87430c3f1b026e258a83f61
Admin <admin@milads-MacBook-Pro.local>
2017-08-18 00:34:26 -0400

    home.html and api route update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,35 +16,38 @@ module.exports=function(app,passport){
             loan_type: req.body.loan_type,
             balance: req.body.balance,
             interest_rate: req.body.interest,
-            minimum_Payment: req.body.minimum_Payment
-        }
-        db.Loan.create(newLoan).then(function(data){
-            res.json(data);
-        });
-      });
-
-
-      app.post("/api/newLoan", function(req, res) {
-        var newLoan={
-            name: req.body.name,
-            loan_type: req.body.loan_type,
-            balance: req.body.balance,
-            interest_rate: req.body.interest,
             minimum_Payment: req.body.minimum_Payment,
             UserId:req.user.dataValues.id
         }
-        db.Loan.create(newLoan).then(function(data){
+        db.Loan.create(newLoan)
+        .then(function(data){
             res.json(data);
-        });
+        })
+        .catch(function(err){
+            res.json(err);
+        })
       });
+
+
 
 
       app.post("/api/delete", function(req, res) {
           db.Loan.destroy({where:{id:req.body.id}}).then(function(data){
               res.json(data);
           });
-
-
       });
+
+      app.post("/api/update", function(req, res) {
+        var updateLoan={
+            name: req.body.name,
+            loan_type: req.body.loan_type,
+            balance: req.body.balance,
+            interest_rate: req.body.interest,
+            minimum_Payment: req.body.minimum_Payment,
+        }
+        db.Loan.update(updateLoan,{where:{id:req.body.id}}).then(function(data){
+            res.json(data);
+        });
+    });
 
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,7 +23,7 @@ module.exports = function(app,passport) {
 
 
 
-    app.post("/login",passport.authenticate('local', { failureRedirect: '/login' }),function(req,res){
+    app.post("/login",passport.authenticate('local', { failureRedirect: '/' }),function(req,res){
         res.json(true);
     });
 
